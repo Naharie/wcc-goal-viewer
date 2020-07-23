9fa41c7b649cfa684ab471a7d96c3f2e8c3a4272
KingDavid12 <childrenholmes@gmail.com>
2020-07-23 11:05:38 -0600

    Began updating project to use React

---
+++ src/models.d.ts
@@ -0,0 +1,78 @@
+﻿interface Goal
+{
+	text: string;
+	id: string;
+}
+
+interface PrimaryReference
+{
+	/**
+	 * The id of the primary goal that is being referenced.
+	 */
+	goal: string;
+	/**
+	 * The ids of the sub goals that are being referenced.
+	 */
+	subGoals: string[];
+}
+
+interface TrackGoal extends Goal
+{
+	/**
+	 * The primary goals this track goal references.
+	 */
+	references: PrimaryReference[];
+}
+
+interface CourseGoal extends Goal
+{
+	/**
+	 * The track goals this course goal references.
+	 */
+	references: string[];
+}
+
+type CourseId = "HMN" | "THL" | "PHL" | "MTH/SCI" | "MUS/ART" | "TRV" | "LAT" | "OLP";
+
+
+interface PrimaryGoal
+{
+	text: string;
+	id: string;
+	children: Goal[];
+}
+
+interface Track
+{
+	track: CourseId;
+	goals: TrackGoal[];
+}
+
+type Semester = CourseGoal[];
+
+interface Year
+{
+	// Which year this is.
+	yearNumber: 100 | 200 | 300 | 400,
+	// Goals for semester 1
+	semester1: Semester;
+	// Goals for semester 2
+	semester2: Semester;
+}
+
+interface Course
+{
+	course: CourseId;
+	// The four years during which the course could be assigned (100, 200, 300, 400).
+	years: [ Year, Year, Year, Year ]
+}
+
+interface JsonData
+{
+	// Left column
+	primaryGoals: PrimaryGoal[],
+	// Center column
+	trackGoals: Track[],
+	// Right column
+	courseGoals: Course[]
+};
---
+++ src/scrollbar.ts
@@ -0,0 +1,42 @@
+﻿let scrollBarWidth = -1;
+
+/**
+ * Computes the width of the browser's builtin scrollbar.
+ */
+const getScrollBarWidth = function ()
+{
+	if (scrollBarWidth != -1)
+	{
+		return (scrollBarWidth);
+	}
+
+	const scrollDiv = document.createElement("div");
+	scrollDiv.classList.add("scrollbar-measure");
+	document.body.appendChild(scrollDiv);
+
+	scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+	document.body.removeChild(scrollDiv);
+
+	return (scrollBarWidth);
+};
+
+/**
+ * Adds events to only show the scrollbar when the user's mouse is over the target element.
+ * @param element The container element.
+ */
+const createScrollWrapper = function (element: HTMLElement)
+{
+	const child = element.firstElementChild;
+
+	if (child == null || !(child instanceof HTMLElement))
+	{
+		return;
+	}
+
+	child.style.paddingRight = getScrollBarWidth() + "px";
+
+	child.addEventListener("mouseover", () => child.style.paddingRight = "0");
+	child.addEventListener("mouseout", () => child.style.paddingRight = getScrollBarWidth() + "px");
+};
+
+export default {};
