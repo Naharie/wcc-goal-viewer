26ed3fa3f5ec4f79cb1f679109fd0bf7511e25e4
Naharie <oneofthosewhoescaped@gmail.com>
2020-08-04 12:58:06 -0600

    Started work on a basic editor

--- src/components/TrackGoal.tsx
+++ src/components/TrackGoal.tsx
@@ -23,19 +23,50 @@ const renderReferences = (references: PrimaryReference[]) =>
                 reference.goal :
                 reference.goal + " " + reference.subGoals.join(", ")
         ).join("; ");
-    
+
     return ("(" + text + ")");
 };
 
-const TrackGoal = ({ goal, scores, selected, setSelected } : TrackGoalProps) =>
-    <li
-        key={goal.id}
-        className={list("mb-1-3", selected ? "selected" : "")}
-        onClick={() => setSelected(!selected)}
-    >
-        {goal.text} {renderReferences(goal.references)}
-        
-        <ScoreList scores={scores} />
-    </li>;
+const TrackGoal = ({ goal, scores, selected, setSelected }: TrackGoalProps) =>
+{
+    const toggleSelection = (event: React.MouseEvent<HTMLLIElement>) =>
+    {
+        if (event.target !== event.currentTarget)
+        {
+            return;
+        }
+
+        setSelected(!selected);
+    }
+
+    return (
+        <li
+            key={goal.id}
+            className={list("mb-1-3", selected ? "selected" : "")}
+            onClick={toggleSelection}
+        >
+            {goal.text} {renderReferences(goal.references)}
+            <ScoreList scores={scores} />
+        </li>
+    );
+
+    /*
+    return (
+        <li
+            key={goal.id}
+            className={list("mb-1-3", selected ? "selected" : "")}
+            onClick={toggleSelection}
+        >
+            <textarea
+                className={list("w-100", selected ? "selected" : "non-selected")}
+                defaultValue={goal.text}
+            ></textarea>
+            {renderReferences(goal.references)}
+
+            <ScoreList scores={scores} />
+        </li>
+    );
+    */
+};
 
 export default TrackGoal;
