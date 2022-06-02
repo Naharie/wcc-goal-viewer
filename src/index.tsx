b78595d9d9d6ba237c896987e6decc834023fbb3
Naharie <oneofthosewhoescaped@gmail.com>
2022-06-02 14:42:09 -0600

    Clean up and reorganize the basic structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
+/*
+https://github.com/rommguy/react-custom-scroll
+https://github.com/Naharie/wcc-goal-viewer
+https://react-redux.js.org/introduction/getting-started
+*/
 
 root.render(
   <React.StrictMode>
@@ -16,8 +21,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
