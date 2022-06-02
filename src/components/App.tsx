62d851f6e85fa379453349e5b02afb18ab9c327c
Naharie <oneofthosewhoescaped@gmail.com>
2022-06-02 15:25:27 -0600

    Move the example to using styled components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,43 @@
-import React from "react";
-import '../App.css';
+import React, { PropsWithChildren } from "react";
+import styled from "styled-components";
 
-const App = () =>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+const App = styled.div`
+    text-align: center;
+`;
+
+const AppHeader = styled.header`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+`;
+
+const AppLinkBase = styled.a`
+    color: rgb(112, 76, 182);
+`;
+
+const AppLink = ({ href, children }: PropsWithChildren<{ href: string }>) =>
+    <AppLinkBase href={href} target="_blank" rel="noopener noreferrer">{children}</AppLinkBase>;
+
+const AppCode = styled.code`
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+`;
+
+export default () =>
+    <App>
+      <AppHeader>
+        <p>Edit <AppCode>src/App.tsx</AppCode> and save to reload.</p>
         <span>
           <span>Learn </span>
-          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            React
-          </a>
+          <AppLink href="https://reactjs.org/">React</AppLink>
           <span>, </span>
-          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux
-          </a>
+          <AppLink href="https://redux.js.org/">Redux</AppLink>
           <span>, </span>
-          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux Toolkit
-          </a>
+          <AppLink href="https://redux-toolkit.js.org/">Redux Toolkit</AppLink>
           ,<span> and </span>
-          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-            React Redux
-          </a>
+          <AppLink href="https://react-redux.js.org/">React Redux</AppLink>
         </span>
-      </header>
-    </div>;
-
-export default App;
+      </AppHeader>
+    </App>;
