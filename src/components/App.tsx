ec8c97aa7126341f770450f52fb55c245bc28733
Naharie <oneofthosewhoescaped@gmail.com>
2022-06-02 14:48:32 -0600

    Removed more pieces to create a more clean slate to work with

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import { Counter } from '../features/counter/Counter';
-import './App.css';
+import React from "react";
+import '../App.css';
 
 const App = () =>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
