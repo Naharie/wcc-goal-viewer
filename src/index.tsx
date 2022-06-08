a2c8ebc4986995071e9a3ac871c9a233be29d1d6
Naharie <oneofthosewhoescaped@gmail.com>
2022-06-08 11:40:27 -0600

    Reple react-custom-scroll with simplebar-react

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./css/index.css";
 
 import { store } from "./store";
-
 import { Provider } from "react-redux";
+
 import App from "./components/App";
 
+import "simplebar-react/dist/simplebar.min.css";
+import "./css/index.css";
+
 /*
-https://github.com/rommguy/react-custom-scroll
 https://redux.js.org/usage/deriving-data-selectors
 https://github.com/Naharie/wcc-goal-viewer/tree/06507ed1ec527f8e384eb3c358b273c7af595909
 https://github.com/Naharie/wcc-goal-viewer/tree/06507ed1ec527f8e384eb3c358b273c7af595909/src
