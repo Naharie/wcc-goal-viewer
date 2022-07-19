3f64d6496f47ab50ca400b2768400d00a43b5751
Naharie <oneofthosewhoescaped@gmail.com>
2022-07-19 01:01:36 -0600

    Rewrite the validator to be simpler

--- src/validators/trackGoalReferencesValidator.ts
+++ src/validators/trackGoalReferencesValidator.ts
@@ -1,81 +1,82 @@
 import useHighlight from "../data/highlight";
 import { fromRomanNumeral } from "../utilities/roman-numerals";
 
-const validator = (references: string) =>
+const validator = (text: string) =>
 {
-    const parts = references.split(";");
-    const numerals = new Set([ "I", "V", "X", "L", "C", "D", "M" ]);
+    const references = text.split(";");
 
+    const numerals = new Set([ "I", "V", "X", "L", "C", "D", "M" ]);
     const isLetter = (char: string) => char >= "a" && char <= "z";
+
     let lastValue = 0;
 
-    for (let i = 0; i < parts.length; i++)
+    for (let i = 0; i < references.length; i++)
     {
-        let part = parts[i];
-        if (part.trim() === "" && i === parts.length - 1) continue;
-
-        if (i > 0 && !part.startsWith(" "))
+        const pieces = references[i].split(" ");
+        
+        if (i > 0)
         {
-            return ("Put exactly one space between a semicolon and the next reference.");
-        }
-
-        let [primaryGoal, ...childGoals] = part.split(" ");
-        let childSegment = childGoals.join(" ");
-
-        primaryGoal = primaryGoal.trim();
-
-        for (let j = 0; j < primaryGoal.length; j++)
-        {
-            if (!numerals.has(primaryGoal[j]))
+            if (pieces[0] !== "")
             {
-                if (primaryGoal[j] === ",")
-                {
-                    return ("Top level references are separated by semicolons and not commas.");
-                }
-
-                return ("References must be formed using Roman numerals.");
+                return ("Put exactly one space after each semicolon and before the next reference.");
             }
+
+            pieces.splice(0, 1);
+        }
+        
+        const reference = pieces[0];
+        const subReferences = pieces.slice(1).join(" ").split(",");
+        
+        if (reference === "") continue;
+
+        // Is made up of Roman Numerals
+        if (!reference.split("").every(char => char === " " || numerals.has(char)))
+        {
+            return (`References must me composed using Roman numerals.`);
         }
 
-        if (!Object.hasOwn(useHighlight.getState().curriculumGoals, primaryGoal))
+        // Exists
+        if (!Object.hasOwn(useHighlight.getState().curriculumGoals, reference))
         {
-            return (`The reference ${primaryGoal} does not, but must, point to an existing curriculum goal.`);
+            return (`The reference ${reference} does not, but must, point to an existing curriculum goal.`);
+        }
+        
+        // Sort in Ascending Order
+        {
+            let newValue = fromRomanNumeral(reference);
+
+            if (newValue < lastValue)
+            {
+                debugger;
+                return ("References must be sorted in ascending order.");
+            }
+
+            lastValue = newValue;
         }
 
-        const childParts = childSegment.split(",");
-
-        for (let j = 0; j < childParts.length; j++)
+        // Child References
+        for (let i = 0; i < subReferences.length; i++)
         {
-            let part = childParts[j];
+            let part = subReferences[i];
             if (part === "") continue;
 
-            if (j > 0 && !part.startsWith(" "))
+            if (i > 0 && !part.startsWith(" "))
             {
                 return ("Put exactly one space between a comma and the next child reference.");
             }
 
-            const reference = j === 0 ? part[0] : part[1];
+            const subReference = i === 0 ? part[0] : part[1];
             
-            if ((j > 0 && part.length < 2) || part.length > 2 || !isLetter(reference))
+            if ((i > 0 && part.length < 2) || part.length > 2 || !isLetter(subReference))
             {
                 return ("Child references must consist of exactly one space and one lowercase letter.");
             }
             
-            if (!Object.hasOwn(useHighlight.getState().curriculumGoals[primaryGoal], reference))
+            if (!Object.hasOwn(useHighlight.getState().curriculumGoals[reference], subReference))
             {
-                return (`The reference ${primaryGoal} ${reference} does not, but must, point to an existing curriculum sub goal.`);
+                return (`The reference ${reference} ${subReference} does not, but must, point to an existing curriculum sub goal.`);
             }
         }
-
-        let newValue = fromRomanNumeral(primaryGoal);
-
-        if (newValue < lastValue)
-        {
-            debugger;
-            return ("References must be sorted in ascending order.");
-        }
-
-        lastValue = newValue;
     }
 
     return true;
